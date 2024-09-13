036a593b6e7053c67cf6a08c037b99a584e3f2bb
Daniel <dbrowne@logik.io>
2024-09-13 09:44:23 -0500

    mock testing progress

--- src/test/java/io/logik/graph_ql_poc/service/GitHubClientServiceTest.java
+++ src/test/java/io/logik/graph_ql_poc/service/GitHubClientServiceTest.java
@@ -2,44 +2,77 @@ package io.logik.graph_ql_poc.service;
 
 import io.logik.graph_ql_poc.models.github.Repositories;
 
+import io.logik.graph_ql_poc.models.github.RepositoryResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.graphql.client.GraphQlClient;
+import org.springframework.graphql.client.HttpGraphQlClient;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class GitHubClientServiceTest {
 
-    @Autowired
-    private GitHubClientService gitHubClientService;
-    // Erase before adding - fill with your Git PAT to test
-    private String gitToken = "";
+    @Mock
+    private HttpGraphQlClient mockGraphQlClient;
 
-    @BeforeEach
-    void setup() {
-        if (gitToken == null || gitToken.isEmpty()) {
-            throw new RuntimeException("Token empty or not found, requests will not work.  " +
-                    "Make sure to edit line 18 by pasting your github PAT. Must have repository read scope");
-        }
-        gitHubClientService.initializeClientWithCorrectEndpoint();
-        gitHubClientService.setClientToken(gitToken);
-    }
+    @InjectMocks
+    private GitHubClientService gitHubClientService;
 
     @Test
     void getRepository() {
+        mockGraphClient();
+
         Repositories response = gitHubClientService.getRepository(3);
-        assertThat(response.getTotalCount()).isEqualTo(5);
-        assertThat(response.getNodes()[0].getNameWithOwner()).isEqualTo("daniel-logik/TestEmptyRepository");
+        assertThat(response.getTotalCount()).isEqualTo(2);
+        assertThat(response.getNodes()[0].getNameWithOwner()).isEqualTo("abc-repoName");
 
     }
 
     @Test
     void getRepositoryOwnerID() {
+        GraphQlClient.RequestSpec requestSpec = Mockito.mock(GraphQlClient.RequestSpec.class);
+        GraphQlClient.RetrieveSyncSpec retrieveSpec = Mockito.mock(GraphQlClient.RetrieveSyncSpec.class);
+
+        when(mockGraphQlClient.documentName("getRepositoryOwner")).thenReturn(requestSpec);
+        when(requestSpec.variable(any(), any())).thenReturn(requestSpec);
+        when(requestSpec.retrieveSync("repositoryOwner.id")).thenReturn(retrieveSpec);
+        when(retrieveSpec.toEntity(String.class)).thenReturn("fakeIdIsHere");
+
+
         String ownerId = gitHubClientService.getRepositoryOwnerID("daniel-logik");
         assertThat(ownerId).isNotNull();
         assertThat(ownerId).isNotEmpty();
+    }
+
+    private void mockGraphClient() {
+        Repositories expectedResponse = new Repositories();
+        RepositoryResponse[] repos = new RepositoryResponse[2];
+        repos[0] = new RepositoryResponse();
+        repos[1] = new RepositoryResponse();
+        repos[0].setNameWithOwner("abc-repoName");
+        repos[1].setNameWithOwner("def-noName");
+        expectedResponse.setNodes(repos);
+        expectedResponse.setTotalCount(repos.length);
+
+        // mock RequestSpec
+        GraphQlClient.RequestSpec requestSpec = Mockito.mock(GraphQlClient.RequestSpec.class);
+        // mock RetriveSpec
+        GraphQlClient.RetrieveSyncSpec retrieveSpec = Mockito.mock(GraphQlClient.RetrieveSyncSpec.class);
+
+        when(mockGraphQlClient.documentName("getRepositories")).thenReturn(requestSpec);
+        when(requestSpec.variables((any()))).thenReturn(requestSpec);
+        when(requestSpec.retrieveSync("viewer.repositories")).thenReturn(retrieveSpec);
+        when(retrieveSpec.toEntity(Repositories.class)).thenReturn(expectedResponse);
+
     }
 
 }
